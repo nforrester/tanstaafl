a77ff94a71ad6461ec3e1441c6160af8b6d6dcb4
Neil Forrester <nforrest@mit.edu>
2009-06-21 23:21:36 -0400

    Finished add method.
    I finished the method for adding to vector-3 s.
    It works.

--- gsfs.lisp
+++ gsfs.lisp
@@ -1,15 +1,21 @@
 ; GNU Space Flight Simulator
 
 (defclass vector-3 ()
-	x
+	(x
 	y
-	z)
+	z))
 
 (defgeneric add (one two))
-(defmethod add ((vector-3 one) (vector-3 two))
-	(with-slots ((ox x) (oy y) (oz y)) one
-		(with-slots ((tx y) (ty y) (tz y)) two
-			
+
+(defmethod add ((one vector-3) (two vector-3))
+	(with-slots ((ox x) (oy y) (oz z)) one
+		(with-slots ((tx x) (ty y) (tz z)) two
+			(let ((result (make-instance 'vector-3)))
+				(with-slots ((rx x) (ry y) (rz z)) result
+					(setf rx (+ ox tx))
+					(setf ry (+ oy ty))
+					(setf rz (+ oz tz))
+					result)))))
 
 (defclass space-object ()
-	mass)
+	(mass))
