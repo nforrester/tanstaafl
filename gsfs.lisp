cf9c69f7ef35415de0601587dafb97580c76f483
Neil Forrester <nforrest@mit.edu>
2009-06-21 23:54:14 -0400

    Multiplication, subtraction, gravitational constant, fleshed out space-object.
    I added the first three, and did the fourth.
    The gravitational constant is an estimate, and should be revised,
    sooner rather than later. I don't have internet at the moment.

--- gsfs.lisp
+++ gsfs.lisp
@@ -5,7 +5,8 @@
 	y
 	z))
 
-(defgeneric add (one two))
+(defgeneric add (one two)
+	:documentation "addition")
 
 (defmethod add ((one vector-3) (two vector-3))
 	(with-slots ((ox x) (oy y) (oz z)) one
@@ -17,5 +18,30 @@
 					(setf rz (+ oz tz))
 					result)))))
 
+(defgeneric mult (scalar something)
+	:documentation "scalar multiplication")
+
+(defmethod mult (scalar (vec vector-3))
+	(with-slots (x y z) vec
+		(let ((result (make-instance 'vector-3)))
+			(with-slots ((rx x) (ry y) (rz z)) result
+				(setf rx (* scalar x))
+				(setf ry (* scalar y))
+				(setf rz (* scalar z))))))
+
+(defgeneric sub (one two)
+	:documentation "subtraction")
+
+(defmethod sub ((one vector-3) (two vector-3))
+	(add one (mult -1 two)))
+
+(defvar *G* 5.9e-11) ; Gravitational constant
+
 (defclass space-object ()
-	(mass))
+	((mass :documentation "Mass measured in kg.")
+	(pos :documentation
+		"position measured in m, cartesian, as a vector-3")
+	(vel :documentation
+		"velocity measured in m/s, cartesian, as a vector-3")
+	(acc :documentation
+		"accelleration measured in m/s/s, cartesian, as a vector-3")))
