9fb4e59fae39229aae62d977348dc247186c9b12
Neil Forrester <nforrest@mit.edu>
2009-07-08 01:08:48 -0400

    Committed go.lisp
    It calls (main-loop) with some interesting arguments. At the moment
    It sets 2 space-objects in orbit around each other, one of which is
    rotating.

---
+++ go.lisp
@@ -0,0 +1,14 @@
+(load "physics.lisp")
+
+(main-loop
+	1
+	(list
+		(make-space-object
+			:mass (/ 4.0 *G*)
+			:pos (make-vector-3 1.0 0.0 0.0)
+			:ang-vel (make-vector-3 0.5 2.0 1.0)
+			:vel (make-vector-3 0.0 0.0 1.0))
+		(make-space-object
+			:mass (/ 4.0 *G*)
+			:pos (make-vector-3 -1.0 0.0 0.0)
+			:vel (make-vector-3 0.0 0.0 -1.0))))
