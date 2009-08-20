3db418be83bbb4d4a77a518933a486ac16bf1eaf
Neil Forrester <nforrest@mit.edu>
2009-08-20 17:31:36 -0400

    Slight cleanup in mfd.lisp
    Changed a minimizing loop to a call to min, and cleaned up a comment.

--- mfd.lisp
+++ mfd.lisp
@@ -25,14 +25,15 @@
 (defmethod compute-size ((mfd mfd) screen-size)
 	(with-slots ((screen-width x) (screen-height y)) screen-size
 		(with-slots ((max-width x) (max-height y)) (slot-value mfd 'max-size)
-			(loop for size in (list (* max-width screen-width) (* max-height screen-height)) minimizing size))))
+			(min (* max-width screen-width) (* max-height screen-height)))))
 
 (defmethod compute-sw-corner ((mfd mfd) screen-size)
 	(with-slots (anchor-point pos) mfd
 		(let ((size (compute-size mfd screen-size)))
 			(sub (mult pos screen-size) (mult size anchor-point)))))
 
-(defmethod draw-2d :around ((mfd mfd) screen-size) ; I give unto thee a viewport. Do what you wish with it.
+;;; I give unto thee a viewport. Do what you wish with it.
+(defmethod draw-2d :around ((mfd mfd) screen-size)
 	(let ((size (compute-size mfd screen-size)) (sw-corner (compute-sw-corner mfd screen-size)))
 		(with-slots (x y) sw-corner
 			(gl-viewport x y size size)))
